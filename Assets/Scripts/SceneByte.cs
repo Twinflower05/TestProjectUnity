9b446bf40441e2f9497f54426c0dfd1b75ddcdfe
Twinflower05 <lei@jernstedt.se>
2026-01-08 15:02:09 +0100

    Scene byte

--- Assets/Scripts/SceneByte.cs
+++ Assets/Scripts/SceneByte.cs
@@ -3,15 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneByte : MonoBehaviour
 {
-    private string Scene2;
-    //namn på scen du byter till
-    public void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        //namn på objekt som triggar scenbyte
         if (other.tag == "Exit")
         {
-            SceneManager.LoadScene(1);
+           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
-
 }
